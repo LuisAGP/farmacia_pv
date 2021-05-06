2668b2a3376a3fb8dca7ab9cb99a076136a0fc29
LuisAGP <luis.lagp4598@gmail.com>
2021-05-06 00:50:30 -0500

    Trabajando en scroll

--- Vista_Inicio.py
+++ Vista_Inicio.py
@@ -2,13 +2,14 @@ from tkinter import *
 
 class Inicio():
     def __init__(self, frame):
+        self.root = frame
         self.main_frame = Frame(frame)
         self.main_frame.pack(fill=BOTH, expand=TRUE)
 
         w = self.get_width() - 25
         h = self.get_height()
 
-        self.canvas = Canvas(self.main_frame)
+        self.canvas = Canvas(self.main_frame, width=200)
         self.canvas.pack(side=LEFT, fill=BOTH, expand=TRUE)
         
         self.scroll = Scrollbar(self.main_frame, orient=VERTICAL, command=self.canvas.yview)
@@ -21,7 +22,8 @@ class Inicio():
         self.canvas.create_window((0, 0), window=self.frame, anchor="nw", width=w)
 
     def get_width(self):
-        return self.main_frame.winfo_width()
+        print(self.main_frame.winfo_reqwidth())
+        return self.main_frame.winfo_reqwidth()
     
     def get_height(self):
         return self.main_frame.winfo_height()
--- main.py
+++ main.py
@@ -16,23 +16,15 @@ def main():
     width = root.winfo_width()
     height = root.winfo_height() - 25
 
-    root.rowconfigure(0, weight=1)
-    root.columnconfigure(0, weight=1)
-
     # Frame lateral 
-    lateral = Frame(root, bg="blue")
-    lateral.grid(row=0, column=0, rowspan=30, columnspan=15)
+    lateral = Frame(root, bg="blue", width=200)
+    lateral.grid(row=0, column=0, rowspan=10, columnspan=3)
     lateral.config(bg="lightblue", relief="groove", bd=3, cursor="") 
     caja = Inicio(lateral)
 
 
     for i in range(50):
         Button(caja.frame, text=f"Boton {i}").pack(fill=X, expand = True)
-
-    f = Frame(root, bg="red")
-    f.grid(row=0, column=16, rowspan=30, columnspan=30)
-
-    Button(f, text="Prueba").pack(fill=BOTH, expand=TRUE)
     
 
     # Menu
